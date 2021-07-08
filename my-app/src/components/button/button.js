4d16d3f553e2ceb990a2ec43caaeae45825ccda0
istvanpuki <puki.istvan92@gmail.com>
2021-07-08 12:51:30 +0200

    o

--- my-app/src/components/button/button.js
+++ my-app/src/components/button/button.js
@@ -1,8 +1,33 @@
 import './button.css';
+// <button onClick= { () => console.log("Clicked") } >{props.text}</button>
+let timer;
+let prevHTML;
+//window.hello="helló";
+//console.log(hello)
+
+function clickHappened (e) {
+    //console.log("Clicked");
+    //console.log(e.target);
+    
+    const goodHTML = "Goodbye";
+    
+    if (e.target.innerHTML !== goodHTML) {
+        prevHTML = e.target.innerHTML;
+        e.target.innerHTML = goodHTML;
+        timer = setTimeout(() => {e.target.innerHTML = prevHTML}, 3000);
+        
+    } else {
+        e.target.innerHTML = prevHTML;
+        clearTimeout(timer);
+        console.log(prevHTML);
+        
+    }
+
+}
 
 function button(props) {
     return (
-        <button>{props.text}</button>
+        <button onClick={clickHappened} >{props.text}</button>
     );
 }
 
